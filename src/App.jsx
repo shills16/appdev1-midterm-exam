069fef4fa522e36ffb4a191feac5404f8a3b6ce0
shills16 <shielamiclat10@gmail.com>
2024-10-19 12:52:16 -0700

    I'm done converting html class and self closing tags into react

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       {/* Intro */}
@@ -44,7 +37,8 @@ function App() {
               </div>
           </div>        
       </div>
-    {/* Cafe Menu */}
+      
+      {/* Cafe Menu */}
       <div id="menu" className="parallax-window" data-parallax="scroll" data-image-src="img/antique-cafe-bg-02.jpg">
           <div className="container mx-auto tm-container py-24 sm:py-48">
               <div className="text-center mb-16">
@@ -53,7 +47,7 @@ function App() {
               <div className="flex flex-col lg:flex-row justify-around items-center">
                   <div className="flex-1 m-5 rounded-xl px-4 py-6 sm:px-8 sm:py-10 tm-bg-brown tm-item-container">
                       <div className="flex items-start mb-6 tm-menu-item">
-                          <img src="img/menu-item-1.jpg" alt="Image" className="rounded-md">
+                          <img src="img/menu-item-1.jpg" alt="Image" className="rounded-md" />
                           <div className="ml-3 sm:ml-6">
                               <h3 className="text-lg sm:text-xl mb-2 sm:mb-3 tm-text-yellow">Hot Cappuccino</h3>
                               <div className="text-white text-md sm:text-lg font-light mb-1">S $8.50</div>
@@ -61,7 +55,7 @@ function App() {
                           </div>                    
                       </div>
                       <div className="flex items-start mb-6 tm-menu-item">
-                          <img src="img/menu-item-2.jpg" alt="Image" className="rounded-md">
+                          <img src="img/menu-item-2.jpg" alt="Image" className="rounded-md" />
                           <div className="ml-3 sm:ml-6">
                               <h3 className="text-lg sm:text-xl mb-2 sm:mb-3 tm-text-yellow">Hot Americano</h3>
                               <div className="text-white text-md sm:text-lg font-light mb-1">S $9.50 . BG #544639</div>
@@ -69,7 +63,7 @@ function App() {
                           </div>                    
                       </div>
                       <div className="flex items-start mb-6 tm-menu-item">
-                          <img src="img/menu-item-3.jpg" alt="Image" className="rounded-md">
+                          <img src="img/menu-item-3.jpg" alt="Image" className="rounded-md" />
                           <div className="ml-3 sm:ml-6">
                               <h3 className="text-lg sm:text-xl mb-2 sm:mb-3 tm-text-yellow">Hot Latte #FC6</h3>
                               <div className="text-white text-md sm:text-lg font-light mb-1">M $11.75</div>
@@ -77,7 +71,7 @@ function App() {
                           </div>                    
                       </div>
                       <div className="flex items-start mb-6 tm-menu-item">
-                          <img src="img/menu-item-4.jpg" alt="Image" className="rounded-md">
+                          <img src="img/menu-item-4.jpg" alt="Image" className="rounded-md" />
                           <div className="ml-3 sm:ml-6">
                               <h3 className="text-lg sm:text-xl tm-text-yellow mb-1">Hot Espresso</h3>
                               <h3 className="text-lg sm:text-xl mb-2 sm:mb-3 tm-text-yellow">Hot Chocolate</h3>
@@ -92,7 +86,7 @@ function App() {
                               <div className="text-white text-md sm:text-lg font-light mb-1">Small $10</div>
                               <div className="text-white text-md sm:text-lg font-light">Large $15</div>
                           </div>
-                          <img src="img/menu-item-5.jpg" alt="Image" className="rounded-md">                   
+                          <img src="img/menu-item-5.jpg" alt="Image" className="rounded-md" />                   
                       </div>
                       <div className="flex items-start justify-end mb-6 tm-menu-item-2">
                           <div className="text-right mr-6">
@@ -100,7 +94,7 @@ function App() {
                               <div className="text-white text-md sm:text-lg font-light mb-1">Small $12.50</div>
                               <div className="text-white text-md sm:text-lg font-light">Large $16.50</div>
                           </div>
-                          <img src="img/menu-item-6.jpg" alt="Image" className="rounded-md">                    
+                          <img src="img/menu-item-6.jpg" alt="Image" className="rounded-md" />                    
                       </div>
                       <div className="flex items-start justify-end mb-6 tm-menu-item-2">
                           <div className="text-right mr-6">
@@ -108,7 +102,7 @@ function App() {
                               <div className="text-white text-md sm:text-lg font-light mb-1">Small $14</div>
                               <div className="text-white text-md sm:text-lg font-light">Large $18</div>
                           </div>   
-                          <img src="img/menu-item-7.jpg" alt="Image" className="rounded-md">                 
+                          <img src="img/menu-item-7.jpg" alt="Image" className="rounded-md" />                 
                       </div>
                       <div className="flex items-start justify-end mb-6 tm-menu-item-2">                    
                           <div className="text-right mr-6">
@@ -116,7 +110,7 @@ function App() {
                               <h3 className="text-lg sm:text-xl mb-2 sm:mb-3 tm-text-yellow">Iced Mocha</h3>
                               <div className="text-white text-md sm:text-lg font-light">Small $10 . Large $15</div>
                           </div> 
-                          <img src="img/menu-item-8.jpg" alt="Image" className="rounded-md">                   
+                          <img src="img/menu-item-8.jpg" alt="Image" className="rounded-md" />                   
                       </div>
                   </div>
               </div>
@@ -179,6 +173,7 @@ function App() {
                 </div>                
             </footer>
         </div>        
+      </div>
     </>
   )
 }
